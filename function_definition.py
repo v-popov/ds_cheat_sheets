bd55233f611ba5c21448eb06779315ca8ab800b9
victor <victorvtf@gmail.com>
2021-03-07 15:38:11 -0800

    function definition cheatsheet added

---
+++ function_definition.py
@@ -0,0 +1,11 @@
+def my_func(some_arg: int = 5) -> str:
+    """
+    Function for crazy processing of some_arg input argument
+
+    :param some_arg: int
+        Some argument description
+    :return: str
+        Function output description
+    """
+	
+	some_crazy_preprocessing()
